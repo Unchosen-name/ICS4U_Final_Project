376464c534573a7fe0fd5ec1d31ec9e81c0f268b
Alice <34670205+AliceCSY@users.noreply.github.com>
2018-01-18 12:41:35 -0500

    Update InformationSystem.java

--- InformationSystem.java
+++ InformationSystem.java
@@ -1,60 +1,211 @@
-public class InformationSystem{
-  
-  private UserDatabase users;
-  private ProgramDatabase programs;
-  private User currentUser;
-  
-  //Constructor for InformationSystem. Initializes all the needed managers and databases
-  public InformationSystem(){
-    users = new Userdatabase();
-    programs = new ProgramDatabase();
-  }
-  
-  /*
-  1. log in as stu
-  2. log in as admin
-  3. log in as guest
-  4. register as stu
-  5. register as admin
-  */
-  
-  public void displayMenu(){
-    
-  }
-  
-public void processMenuChoice () {
-	int choice;
+import java.io.*;
+import java.util.*;
 
-	do {
-		try {
-			System.out.println("Enter your choice: ");
-			choice = sc.nextInt();
-		} catch (InputMismatchException ime) {
-			sc.nextLine();
-			System.out.println("Invalid input.");
-		}
-	} while (!(choice > 0 && choice <= 5));
+public class InformationSystem {
+	private static String MENU_FILE = "main_menu.txt";
+	private UserDatabase users;
+	//private ProgramDatabase programs;
+	private User currentUser;
+	private String[] menuOptions;
+	private int numMenuOptions;
+	private Scanner sc;
 
-	switch (choice) {
-		case 1:
-			//log in as student
-			break;
-		case 2:
-			//log in as admin
-			break;
-		case 3:
-			//log in as guest
-			break;
-		case 4:
-			createStudentByStandardInput();
-			break;
-		case 5:
-			//register as admin
-			break;
+	public InformationSystem(){
+		users = new UserDatabase();
+		//programs = new ProgramDatabase();
+		sc = new Scanner(System.in);
+		loadMenu();
+		displayMenu();
 	}
-}
-  
-  public static void main(){
-    
-  }
+
+	public void loadMenu () {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(MENU_FILE));
+			numMenuOptions = Integer.parseInt(in.readLine());
+			menuOptions = new String[numMenuOptions];
+			for (int i = 0; i < numMenuOptions; i++) {
+				menuOptions[i] = in.readLine();
+			}
+			in.close();
+		} catch (IOException iox) {
+			System.out.println("Error reading main menu from file.");
+		}
+	}
+
+	public void displayMenu () {
+		for (int i = 0; i < numMenuOptions; i++) {
+			System.out.println(menuOptions[i]);
+		}
+		processMenuChoice();
+	}
+
+	public void processMenuChoice () {
+		int choice = -1;
+
+		do {
+			try {
+				System.out.println("Enter your choice: ");
+				choice = sc.nextInt();
+				sc.nextLine();
+			} catch (InputMismatchException ime) {
+				sc.nextLine();
+				System.out.println("Invalid input.");
+			}
+		} while (!(choice > 0 && choice <= numMenuOptions));
+
+		switch (choice) {
+			case 1:
+				logInStudent();
+				break;
+			case 2:
+				logInAdmin();
+				break;
+			case 3:
+				currentUser = UserDatabase.getGuest();
+				break;
+			case 4:
+				createStudentAccount();
+				break;
+			case 5:
+				createAdminAccount();
+				break;
+		}
+
+		if (currentUser != null) {
+			currentUser.displayMenu();
+		}
+	}
+
+	public void logInStudent () {
+		String username, password;
+
+		System.out.println("Username: ");
+		username = sc.nextLine();
+		System.out.println("Password:" );
+		password = sc.nextLine();
+
+		while (users.searchStudent(username, password) == null) {
+			System.out.println("Invalid username or password!");
+			System.out.println("Username: ");
+			username = sc.nextLine();
+			System.out.println("Password:" );
+			password = sc.nextLine();
+		}
+
+		currentUser = users.searchStudent(username, password);
+	}
+
+
+	public void logInAdmin () {
+		String username, password;
+
+		System.out.println("Username: ");
+		username = sc.nextLine();
+		System.out.println("Password:" );
+		password = sc.nextLine();
+
+		while (users.searchAdmin(username, password) == null) {
+			System.out.println("Invalid username or password!");
+			System.out.println("Username: ");
+			username = sc.nextLine();
+			System.out.println("Password:" );
+			password = sc.nextLine();
+		}
+
+		currentUser = users.searchAdmin(username, password);
+	}
+
+	public void createStudentAccount () {
+		String username, password, firstName, lastName, oen, studentNum;
+		int numCourses = 0;
+
+		System.out.print("Username: ");
+		username = sc.nextLine();
+		while (!users.checkStudentUsername(username)) {
+			System.out.println("Username unavailable. Please enter a new one.");
+			System.out.print("Username: ");
+			username = sc.nextLine();
+		}
+		System.out.print("Password: ");
+		password = sc.nextLine();
+		System.out.print("First Name: ");
+		firstName = sc.nextLine();
+		System.out.print("Last Name: ");
+		lastName = sc.nextLine();
+		System.out.print("OEN: ");
+		oen = sc.nextLine();
+		System.out.print("Student Number: ");
+		studentNum = sc.nextLine();
+		do {
+			try {
+				System.out.print("How many courses are you taking? ");
+				numCourses = sc.nextInt();
+				sc.nextLine();
+			} catch (InputMismatchException ime) {
+				sc.nextLine();
+				System.out.println("Invalid input.");
+			}
+		} while (!(numCourses > 0));
+
+		ArrayList<ActiveCourse> courseList = new ArrayList<>();
+
+		for (int i = 0; i < numCourses; i++) {
+			String courseCode;
+			int mark = -1;
+
+			System.out.print("Course: ");
+			courseCode = sc.nextLine();
+			while (!CourseTracker.isValidCourse(courseCode)) {
+				System.out.println("That course is not offered at AY Jackson.");
+				System.out.print("Enter a new course: ");
+				courseCode = sc.nextLine();
+			}
+
+			do {
+				try {
+					System.out.print("Mark: ");
+					mark = sc.nextInt();
+					sc.nextLine();
+				} catch (InputMismatchException ime) {
+					sc.nextLine();
+					System.out.println("Invalid input.");
+				}
+				if (!(mark >= 0 && mark <= 100)) {
+					System.out.println("Invalid mark.");
+				}
+			} while (!(mark >= 0 && mark <= 100));
+
+			courseList.add(new ActiveCourse(courseCode, mark));
+		}
+
+		Student student = new Student(username, password, firstName, lastName, oen, studentNum, new CourseTracker(courseList), false);
+		users.addStudent(student);
+
+		System.out.println("Registration successful.");
+		displayMenu();
+	}
+
+	public void createAdminAccount () {
+		System.out.print("Username: ");
+		String username = sc.nextLine();
+		while (!users.checkAdminUsername(username)) {
+			System.out.println("Username unavailable. Please enter a new one.");
+			System.out.print("Username: ");
+			username = sc.nextLine();
+		}
+		System.out.print("Password: ");
+		String password = sc.nextLine();
+		System.out.print("Admin Number: ");
+		String adminNumber = sc.nextLine();
+
+		Admin admin = new Admin(username, password, adminNumber);
+		users.addAdmin(admin);
+
+		System.out.println("Registration successful.");
+		displayMenu();
+	}
+
+	public static void main (String[] args) {
+		InformationSystem is = new InformationSystem();
+	}
 }
